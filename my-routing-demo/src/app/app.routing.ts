bbf420ad9d536424e51db8b71c30876c772fa2b8
Rafael Barros <rafaelbarros.df@gmail.com>
2017-09-18 14:32:05 -0300

    Update routing

--- my-routing-demo/src/app/app.routing.ts
+++ my-routing-demo/src/app/app.routing.ts
@@ -1,11 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { AddUserComponent } from './add-user.component';
+import { UserDetailsComponent } from "./user-details.component";
+
 
 // define my routes
 const routes : Routes = [
   { path : '', component : HomeComponent},  // default route
-  { path : 'add-user', component : AddUserComponent}
+  { path : 'add-user', component : AddUserComponent},
+  { path : 'user-details/:name', component : UserDetailsComponent}
 ];
 
 // configure routes for root module
