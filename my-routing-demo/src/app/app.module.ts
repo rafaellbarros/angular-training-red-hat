ee859ff97ee4751b57d6c5319a6ebf51c648b9e0
Rafael Barros <rafaelbarros.df@gmail.com>
2017-09-18 14:15:48 -0300

    Update app.module

--- my-routing-demo/src/app/app.module.ts
+++ my-routing-demo/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from "./home.component";
+import { AddUserComponent } from "./add-user.component";
+import { routing } from "./app.routing";
+import { LocationStrategy, HashLocationStrategy } from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddUserComponent 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [ { provide: LocationStrategy, useClass: HashLocationStrategy} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
