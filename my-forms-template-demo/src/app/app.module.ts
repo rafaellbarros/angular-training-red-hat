340adc275482ed2565e9e6069e4ebbecb43c9a83
Rafael Barros <rafaelbarros.df@gmail.com>
2017-09-18 16:01:41 -0300

    Add form

--- my-forms-template-demo/src/app/app.module.ts
+++ my-forms-template-demo/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { UserComponent } from './user/user.component';
     UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-forms-template-demo/src/app/user/user.component.ts
+++ my-forms-template-demo/src/app/user/user.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
+  usersList: Array<User> = new Array();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: any) {
+    
+        let formValue = form.value;
+    
+        console.log("First name: " + formValue.theFirstName);
+        console.log("Last name: " + formValue.theLastName);
+        console.log("City: " + formValue.theCity);
+    
+        // create user based on form data
+        let tempUser: User = new User(formValue.theFirstName,
+                            formValue.theLastName,
+                            formValue.theCity);
+    
+        // add the new user to our list
+        this.usersList.push(tempUser);
+    
+        // reset the form
+        form.reset();
+      }
+
 }
+
+
+class User {
+  
+    constructor(private firstName: string,
+                private lastName: string,
+                private city: string) { }
+  
+  }
