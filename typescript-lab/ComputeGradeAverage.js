d8ce0afee9bd25ce3555d685ae5d5fa4d5a2fd45
Rafael Barros <rafaelbarros.df@gmail.com>
2017-09-18 11:37:35 -0300

    Initial

---
+++ typescript-lab/ComputeGradeAverage.js
@@ -0,0 +1,7 @@
+// define the array
+var grades = [98, 75, 83, 69];
+// loop thru array
+var i;
+for (i = 0; i < grades.length; i++) {
+    console.log(grades[i]);
+}
---
+++ typescript-lab/ComputeGradeAverage.ts
@@ -0,0 +1,9 @@
+// define the array
+let grades : number[] = [98, 75, 83, 69];
+
+// loop thru array
+let i : number;
+
+for (i=0; i < grades.length; i++) {
+  console.log(grades[i]);
+}
